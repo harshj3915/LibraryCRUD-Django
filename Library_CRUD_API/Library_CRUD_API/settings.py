a0d075d12c538f321c384e44ba2f08859bea07ae
harshj3915 <hjain3915@gmail.com>
2023-05-20 00:47:39 +0530

    19/05/23-Made basic model of Book

--- Library_CRUD_API/Library_CRUD_API/settings.py
+++ Library_CRUD_API/Library_CRUD_API/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'REST_API',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Library_CRUD_API/REST_API/models.py
+++ Library_CRUD_API/REST_API/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    book_Id = models.PositiveIntegerField(primary_key=True)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    description = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.title} : {self.book_Id}'
+    
+    
+
+    
+
