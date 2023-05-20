b7c4ef210665b920d3e15427cdf77c0f3c94b786
harshj3915 <hjain3915@gmail.com>
2023-05-21 01:23:34 +0530

    Added delete and Put

--- Library_CRUD_API/REST_API/urls.py
+++ Library_CRUD_API/REST_API/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('books/', views.BookList),
+    path('books/<int:book_Id>/', views.BookDetail),
 ]
--- Library_CRUD_API/REST_API/views.py
+++ Library_CRUD_API/REST_API/views.py
@@ -8,6 +8,8 @@ from rest_framework.decorators import api_view
 from django.views.decorators.csrf import csrf_exempt
 
 
+
+
 @api_view(['GET','POST'])
 @csrf_exempt
 def BookList(request):
@@ -21,6 +23,25 @@ def BookList(request):
             serializer.save()
             return Response(data=serializer.data,status=status.HTTP_201_CREATED)
         else:
-            return Response(data=request.data,status=status.HTTP_406_NOT_ACCEPTABLE)
+            return Response(serializer.error_messages,status=status.HTTP_406_NOT_ACCEPTABLE)
 
+@api_view(['GET','PUT','DELETE'])
+@csrf_exempt
+def BookDetail(request,book_Id):
+    try:
+        book=Book.objects.get(book_Id=book_Id)
+    except Book.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer=BookSerializer(book,many=False)
+        return JsonResponse(serializer.data,safe=False)
+    elif request.method == 'PUT':
+        serializer=BookSerializer(book,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.error_messages,status=status.HTTP_304_NOT_MODIFIED)
+    elif request.method == 'DELETE':
+        book.delete()
+        return Response(status=status.HTTP_410_GONE)
     
